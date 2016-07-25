7da63c91dfba9e86f2fba22128981f8a0059434e
rmoj <rmoj99@gmail.com>
2016-07-25 15:06:28 -0400

    1. Switched placeholders for username and password textfields 2. Added name and  pass textfield and error label outlets to LoginViewController

--- Midterm (Storyboard)/LoginViewController.m
+++ Midterm (Storyboard)/LoginViewController.m
@@ -9,6 +9,9 @@
 #import "LoginViewController.h"
 
 @interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UILabel *errorLabel;
 
 @end
 
