0cd82e9ee94610b1f6c3b6be4fe7bc1c5c909357
rmoj <rmoj99@gmail.com>
2016-07-27 09:32:28 -0400

    connected EventsDetailViewController to EventsListViewController Deleted CoreLocation methods in EventDetailViewController

--- Midterm (Storyboard)/EventDetailViewController.m
+++ Midterm (Storyboard)/EventDetailViewController.m
@@ -11,12 +11,11 @@
 @import MapKit;
 
 
-@interface EventDetailViewController () <CLLocationManagerDelegate, MKMapViewDelegate>
+@interface EventDetailViewController () <MKMapViewDelegate>
 
 @property (strong, nonatomic) CLLocationManager *locationManager;
 @property (strong, nonatomic) IBOutlet MKMapView *mapView;
 @property (assign, nonatomic) BOOL shouldZoomIntoUser;
-@property (strong,nonatomic) MKPlacemark *placemark;
 
 @end
 
@@ -29,24 +28,7 @@
     self.eventOrganizerLabel.text = [NSString stringWithFormat:@"Organized by: %@ ", self.event.eventOrganizer];
     self.eventDateLabel.text = [NSString stringWithFormat:@"On : %@ ", self.event.eventDate];
     
-    self.shouldZoomIntoUser = YES;
-    
-    self.locationManager = [[CLLocationManager alloc] init];
-    self.locationManager.delegate = self;
-    
     self.mapView.delegate = self;
-    //   self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-    //   self.locationManager.distanceFilter = 50;
-    
-    if ([CLLocationManager locationServicesEnabled]) {
-        
-        if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined) {
-            
-            [self.locationManager requestWhenInUseAuthorization];
-        }
-        
-    }
-    
     
     [self showEventMap];
     
@@ -58,42 +40,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - CLLocationManagerDelegate
-
-- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
-    NSLog(@"Authorization Changed to: %d", status);
-    
-    if (status == kCLAuthorizationStatusAuthorizedWhenInUse) {
-        NSLog(@"Authorized!");
-        
-        self.mapView.showsUserLocation = YES;
-        self.mapView.showsPointsOfInterest = YES;
-        
-        [self.locationManager startUpdatingLocation];
-        
-        //[self.locationM anager requestLocation];
-    }
-}
-
-- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
-    NSLog(@"New locations: %@", locations);
-    // [self.locationManager stopUpdatingLocation]
-    
-    CLLocation *location = [locations lastObject];
-    
-    if (self.shouldZoomIntoUser) {
-        self.shouldZoomIntoUser = NO;
-        
-        CLLocationCoordinate2D coordinate = location.coordinate;
-        
-        MKCoordinateRegion region = MKCoordinateRegionMake(coordinate, MKCoordinateSpanMake(0.001, 0.001));
-        [self.mapView setRegion:region animated:YES];
-    }
-}
-
-- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
-    NSLog(@"Location error: %@", error.localizedDescription);
-}
 
 #pragma mark - Geocode
 
@@ -108,15 +54,14 @@
             CLPlacemark *topResult = [placemarks objectAtIndex:0];
             
             MKPlacemark *placemark = [[MKPlacemark alloc]initWithPlacemark:topResult];
-            self.placemark = placemark;
             
-            CLLocationCoordinate2D center = CLLocationCoordinate2DMake(self.placemark.coordinate.latitude,self.placemark.coordinate.longitude);
+            CLLocationCoordinate2D center = CLLocationCoordinate2DMake(placemark.coordinate.latitude,placemark.coordinate.longitude);
             
             MKCoordinateSpan span = MKCoordinateSpanMake(0.05, 0.05);
             MKCoordinateRegion regionToDisplay = MKCoordinateRegionMake(center, span);
             [self.mapView setRegion:regionToDisplay];
             
-            [self.mapView addAnnotation:self.placemark];
+            [self.mapView addAnnotation:placemark];
             
         }}];
     
