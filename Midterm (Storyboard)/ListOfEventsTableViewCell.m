70f0e5d499f245c4db03e9ae6bd9305cfe0a954a
Yevhen <fortranibm@gmail.com>
2016-07-28 14:03:00 -0400

    gradient on image added

--- Midterm (Storyboard)/ListOfEventsTableViewCell.m
+++ Midterm (Storyboard)/ListOfEventsTableViewCell.m
@@ -11,11 +11,26 @@
 @implementation ListOfEventsTableViewCell
 
 - (void)awakeFromNib {
-    [super awakeFromNib];
-    // Initialization code
+    [self drawGradient];
+}
+
+- (void)setPetImage:(UIImageView *)petImage {
+    _petImage = petImage;
 }
 
 
+- (void)drawGradient
+{
+    CAGradientLayer *gradient = [CAGradientLayer layer];
+    gradient.frame = self.petImage.bounds;
+    gradient.colors = @[(id)[UIColor clearColor].CGColor,
+                        (id)[UIColor blackColor].CGColor];
+    
+    gradient.startPoint = CGPointMake(0.5f, .25f);
+    gradient.endPoint = CGPointMake(.5f, 1.0);
+    
+    [self.petImage.layer addSublayer: gradient];
+}
 
 
 @end
